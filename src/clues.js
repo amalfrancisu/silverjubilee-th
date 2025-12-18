2db64ef36309670a06f5545792bab14a421f3dbf
Amal <amalfrancisu@gmail.com>
2025-12-18 22:56:12 +0530

    changed clues

--- src/clues.js
+++ src/clues.js
@@ -6,63 +6,75 @@ export const clues = [
     id: 1,
     pathId: "dml",
     subheading: "The First Clue",
-    text: "A habitat, a hirsute host, among, this village of masters old. The denizen warm, eager, when others seemed cold. An agreeable sort, friendly, and easy, at least for PMIRs. An un-resented presence, as a nectar gathering bee, less so maybe for Bummers",
+    text: `A place vast, named after the benefactors and host of this great town where we did dwell.
+Visited maybe twice, definitely once, a robed & taselled occasion before the farewell`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 2,
     pathId: "sabka-katega",
     subheading: "The Second Clue",
-    text: "This is the placeholder text for the second clue. Edit this message in the 'src/clues.js' file.",
+    text: `A place of sharing and celebration, the after hours, of banter and jest.
+Meagre, maybe, the price of sustenance, but at the cost of rest`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 3,
     pathId: "gbm-dismissed",
     subheading: "The Third Clue",
-    text: "This is the placeholder text for the third clue. Edit this message in the 'src/clues.js' file.",
+    text: `Folks much older, maybe richer, however still hungry for the rote, the badge and learning.
+On campus for a few days, their dwelling much nicer, after all the company is paying`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 4,
     pathId: "dcp",
     subheading: "The Fourth Clue",
-    text: "This is the placeholder text for the fourth clue. Edit this message in the 'src/clues.js' file.",
+    text: `Page turners they may not all have been, brain candy, inside the door.
+Out in the sun, eternally youthful friends, perhaps more, coz inside is a bore.`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 5,
     pathId: "bhasad",
     subheading: "The Fifth Clue",
-    text: "This is the placeholder text for the fifth clue. Edit this message in the 'src/clues.js' file.",
+    text: `A habitat, a hirsute host, among, this village of masters old.
+The denizen warm, eager, when others seemed cold.
+An agreeable sort, friendly,  and easy, at least for PMIRs.
+An un-resented presence, as a nectar gathering bee, less so maybe for Bummers.`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 6,
     pathId: "wet-night",
     subheading: "The Sixth Clue",
-    text: "This is the placeholder text for the sixth clue. Edit this message in the 'src/clues.js' file.",
+    text: `Was a cubby hole, a place to inhale, imbibe and take a pause.
+Place has changed, friendships gained, but out lungs a lost cause.`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 7,
     pathId: "fraxi",
     subheading: "The Seventh Clue",
-    text: "This is the placeholder text for the seventh clue. Edit this message in the 'src/clues.js' file.",
+    text: `We used these chambers to share our presentations clearly.
+But any glitches in the shared drive would cost quite dearly.`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 8,
     pathId: "xl-meri-jaan",
     subheading: "The Eighth Clue",
-    text: "This is the placeholder text for the eighth clue. Edit this message in the 'src/clues.js' file.",
+    text: `A boozy shelter, exposed to the late night sky though, on the hallowed high plain of yore,
+Witnessed the running exploits of a shepherd (my bosomy shepherdess, more like it...) king/queen of lore.`,
     backgroundImage: "/thimage.jpg",
   },
   {
     id: 9,
     pathId: "strike",
     subheading: "The Ninth Clue",
-    text: "This is the placeholder text for the ninth clue. Edit this message in the 'src/clues.js' file.",
+    text: `Maybe an Ent* wandered afar, a solitary timbered axis, famous today musically even, but for decades welcoming,
+And sheltered XLers, Waiting for placements, waiting for mates, even waiting occasionally to be dunked unknowing
+*in the land where the shadows lie...in the darkness bind them...one to rule them all.`,
     backgroundImage: "/thimage.jpg",
   },
   {
